f2fe1be530f9e1101933b50669c9799738eeb935
Kawikakn <kknjr16@gmail.com>
2025-04-01 22:28:54 -1000

    contacts table finished in 33 minutes

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,43 +1,23 @@
 import { Col, Container, Row } from 'react-bootstrap';
-import { Contact } from '@/lib/validationSchemas';
 import ContactCard from '@/components/ContactCard';
+import { prisma } from '@/lib/prisma';
+import { getServerSession } from 'next-auth';
+import authOptions from '@/lib/authOptions';
 
-const contacts: Contact[] = [
-  {
-    firstName: 'Philip',
-    lastName: 'Johnson',
-    address: 'POST 307, University of Hawaii',
-    image: 'https://github.com/philipmjohnson.png',
-    description: 'I am a Professor of Information and Computer Sciences at the University of Hawaii, Director'
-      + ' of the Collaborative Software Development Laboratory, and the CEO of OpenPowerQuality.com.',
-  },
-  {
-    firstName: 'Henri',
-    lastName: 'Casanova',
-    address: 'POST 307, University of Hawaii',
-    image: 'https://avatars0.githubusercontent.com/u/7494478?s=460&v=4',
-    description: 'I am originally from France. I maintain a list of reports from my surf sessions. I have proof'
-      + ' that I ran the Hana relay with an actual Team.',
-  },
-  {
-    firstName: 'Kim',
-    lastName: 'Binsted',
-    address: 'POST 307, University of Hawaii',
-    image: 'https://www.ics.hawaii.edu/wp-content/uploads/2013/08/kim_binsted-square-300x300.jpg',
-    description: 'Kim Binsted received her BSc in Physics at McGill (1991), and her PhD in Artificial Intelligence'
-      + ' from the University of Edinburgh (1996). Her thesis topic was the computational modeling and generation of'
-      + ' punning riddles, and her program, JAPE (Joke Analysis and Production Engine), generated puns such as'
-      + ' "What do you call a Martian who drinks beer? An ale-ien!".',
-  },
-];
+export default async function ListContacts() {
+  const session = await getServerSession(authOptions);
+  const contacts = await prisma.contact.findMany({
+    where: {
+      owner: session?.user?.email || '',
+    },
+  });
 
-export default function ListContacts() {
   return (
     <Container className="py-5">
       <h2>List Contacts</h2>
       <Row xs={1} md={2} lg={3} className="g-4">
         {contacts.map((contact) => (
-          <Col key={`Contact-${contact.firstName}`}>
+          <Col key={`Contact-${contact.id}`}>
             <ContactCard contact={contact} />
           </Col>
         ))}
